598689d6898dc14d7625a74069ef56c17515950c
Falk <Falk@DESKTOP-4F218UM>
2025-07-01 14:55:39 +0200

    Switch auf firefox 2

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ firefox_options.add_argument('--no-sandbox')  # Verhindert Sandbox-Probleme
 firefox_options.add_argument('--disable-dev-shm-usage')  # Verhindert Shared Memory-Probleme
 
 # Geckodriver Service
-gecko_driver_path = '/usr/bin/geckodriver'  # Standardpfad für Geckodriver
+gecko_driver_path = '/usr/local/bin/geckodriver'  # Standardpfad für Geckodriver
 service = Service(gecko_driver_path)
 
 # Firefox WebDriver starten
 driver = webdriver.Firefox(service=service, options=firefox_options)
 
 # Beispiel: URL aufrufen
-driver.get('https://www.example.com')
+driver.get('https://www.google.com/?hl=de&zx=1751373259440&no_sw_cr=1')
 
 # Beispiel: Alle Links auf der Seite auslesen
 links = driver.find_elements(By.TAG_NAME, 'a')
